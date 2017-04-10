7ddf69cdb77cb2bd6cade30a0105447b90e66ee4
Alexander Wald <alexander.wald@jumio.com>
2017-04-10 20:33:41 +0200

    Initial

---
+++ halla.ino
@@ -0,0 +1,87 @@
+/*
+  Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo and Micro support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+int led = 9;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+void setup() {
+    // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+}
+
+void loop() { // run over and over
+  /*
+int brightness = 0;    // how bright the LED is
+
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+    if (mySerial.read() <= 0 || mySerial.read() >= 255) {
+      brightness =  mySerial.read();
+      analogWrite(led, brightness);
+    }
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }*/
+    // set the brightness of pin 9:
+
+  analogWrite(led, mySerial.read());
+//  analogWrite(led, Serial.read());
+  delay(50);
+
+    /*
+  analogWrite(led, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(30);
+  */
+}
+
