540a156a86e512fb01f0163081a50f76daa6bece
Alex Wald <wald.alexander@gmail.com>
2019-06-10 17:28:13 +0200

    initial commit

--- halla-arduino.ino
+++ halla-arduino.ino
@@ -3,18 +3,7 @@
  The circuit:
  * RX is digital pin 11 (connect to TX of other device)
  * TX is digital pin 12 (connect to RX of other device)
-
  */
-
-#define DEBUG 1
-
-#ifdef DEBUG
- #define DEBUG_PRINT(x)  Serial.print (x)
- #define DEBUG_PRINTLN(x)  Serial.println (x)
-#else
- #define DEBUG_PRINT(x)
- #define DEBUG_PRINTLN(x)
-#endif
 
 #include <SoftwareSerial.h>
 
@@ -24,6 +13,7 @@ int redLed = 3;           // the PWM pin
 int yellowLed = 5;           // the PWM pin
 int greenLed = 6;           // the PWM pin
 
+int ledCount = 4;           // internal protocol for LED count discovery
 
 int brightness = 0;    // how bright the LED is
 String inString = "";    // string to hold input
@@ -36,66 +26,91 @@ void setup() {
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  mySerial.println("Hello, world?");
  
-      mySerial.println("SET BT NAME Arduino_BT");
-        Serial.begin(9600);                      // connect to the serial port
-         mySerial.println("AT + NAMESvietidlo");
-      mySerial.write("AT + NAMESvietidlo");
+  Serial.begin(9600);                      // connect to the serial port
 
-      Serial.println("SET BT NAME Arduino_BT");
+  setName();
 }
 
 void loop() {
+  int inChar = mySerial.read();
 
-////// COMM IONLY
-//char c;
-//  if (Serial.available()) {
-//    c = Serial.read();
-//    mySerial.print(c);
-//  }
-//  if (mySerial.available()) {
-//    c = mySerial.read();
-//    Serial.print(c);    
-//  }
-
-//////
-
-  
-    int inChar = mySerial.read();
+    //internal protocol for LED count discovery
+    if (inChar == 'q'){
+       mySerial.print(ledCount);
+       Serial.print(ledCount);
+    }
+    
     if (isDigit(inChar)) {
       // convert the incoming byte to a char
       // and add it to the string:
       inString += (char)inChar;
-    }
-    // if you get a newline, print the string,
+    } 
+    // if we get a newline, print the string,
     // then the string's value:
     if (inChar == '\n') {
-      DEBUG_PRINT("Value:");
-      DEBUG_PRINTLN(inString.toInt());
+      Serial.print("Value:");
+      Serial.println(inString.toInt());
 
       brightness = inString.toInt();
       
       if (brightness >= 0 && brightness <= 255) {
         analogWrite(led, brightness);
       } else if (brightness >= 256 && brightness <= 511) {
-          DEBUG_PRINT("assigning redValue: ");
-          DEBUG_PRINT(brightness-256);
+          Serial.print("assigning redValue: ");
+          Serial.print(brightness-256);
         analogWrite(redLed, brightness-256);
       } else if  (brightness >= 512 && brightness <= 767) {
-        DEBUG_PRINT("assigning yellowValue: ");
-        DEBUG_PRINT(brightness-256);
+        Serial.print("assigning yellowValue: ");
+        Serial.print(brightness-256);
         analogWrite(yellowLed, brightness-256);
       } else if  (brightness >= 768 && brightness <= 1023) {
-        DEBUG_PRINT("assigning yellowValue: ");
-        DEBUG_PRINT(brightness-256);
+        Serial.print("assigning yellowValue: ");
+        Serial.print(brightness-256);
         analogWrite(greenLed, brightness-256);
       }
       
-      DEBUG_PRINT("String: ");
-      DEBUG_PRINTLN(inString);
+      Serial.print("String: ");
+      Serial.println(inString);
       // clear the string for new input:
       inString = "";
     } 
+}
+
+void setName() {
+  String nameValue = "";
+
+  if (ledCount > 2) {
+    nameValue = "Halla360";
+  } else {
+      nameValue = "Halla180";
+  }
+  
+  String command(F("AT+NAME"));
+  command += nameValue;
+  doCommandAndEchoResult(command.c_str());
+}
+
+void doCommandAndEchoResult(const char * command)
+//void doCommandAndEchoResult(const char * command, const __FlashStringHelper * meaning)
+
+{
+  // announce command
+  Serial.print(F("Sending command: "));
+  Serial.print(command);
+//  if (meaning != NULL)
+//  {
+//    Serial.print(F(" ("));
+//    Serial.print(meaning);
+//    Serial.print(F(")"));
+//  }
+  Serial.println();
+
+  // send command
+    mySerial.print(command);
+     Serial.print(command);
+
+  // normalize line end
+    Serial.println();
 }
 
